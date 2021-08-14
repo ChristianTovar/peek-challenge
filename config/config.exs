e0a08372d54e204f717b1b2efb7c6decd836f60d
ChristianTovar <tovarchristian21@gmail.com>
2021-08-14 11:52:09 -0500

    chore: add CI tooling

--- config/config.exs
+++ config/config.exs
@@ -27,6 +27,23 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Git hooks configurations
+if Mix.env() == :dev do
+  config :git_hooks,
+    auto_install: true,
+    verbose: true,
+    hooks: [
+      pre_commit: [
+        tasks: [
+          {:cmd, "mix format --check-formatted"},
+          {:cmd, "mix credo"},
+          {:cmd, "mix doctor --summary"},
+          {:cmd, "mix test"}
+        ]
+      ]
+    ]
+end
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
